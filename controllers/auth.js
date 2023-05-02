8f1cc5257ea5abc15551da161ac3bea75ae832dd
lucianoislas <luciano@gruposydem.com>
2023-05-02 00:26:15 -0700

    Fin sección 11 - versión 3.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,9 @@ const { response } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
     const { correo, password } = req.body;
@@ -34,7 +36,7 @@ const login = async(req, res = response) => {
             });
         }
 
-        // TODO Generar el JWT
+        // Generar el JWT
         const token = await generarJWT( usuario.id );
 
         res.json({
@@ -50,6 +52,52 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Tengo que crear el usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+
+            await usuario.save();
+        }
+
+        // Si el usuario está activo en BD
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
